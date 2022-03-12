b63eed0cf87ae087bd7d47a778891515de86550f
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-12 10:17:10 +0700

    Fix wrong mqtt port config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   MQTT_USERNAME: process.env.MQTT_USERNAME,
   MQTT_PASSWORD: process.env.MQTT_PASSWORD,
   MQTT_HOST: process.env.MQTT_HOST,
-  MQTT_PORT: process.env.MQTT_PASSWORD,
+  MQTT_PORT: process.env.MQTT_PORT,
   API_KEY: process.env.API_KEY,
   SECRET: process.env.SECRET
 };
